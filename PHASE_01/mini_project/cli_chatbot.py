698c1cf077ecf7fdc08a4cb7eaf7966676262cc8
shamoon-ahmed <samsonrizzy6@gmail.com>
2026-08-01 15:06:08 +0500

    added comments

--- PHASE_01/mini_project/cli_chatbot.py
+++ PHASE_01/mini_project/cli_chatbot.py
@@ -1,13 +1,12 @@
 # A command-line chatbot that maintains conversation history and
 # uses a system prompt we wrote. We will deploy it to GitHub. 
-# This is your first portfolio piece.
+# This is my first portfolio piece.
 
 from google import genai
 from google.genai import types
 
 from pathlib import Path
 import json
-import os
 
 from dotenv import load_dotenv
 
@@ -27,10 +26,13 @@ config = types.GenerateContentConfig(
     thinking_config=types.ThinkingConfig(thinking_level='medium')
 )
 
+# whenever user quits (says quit or bye), the conversation history saves of that chat
 def save_history(chat):
     
     chat_history = []
 
+    # as chat messages has types.Content() objects which is the way gemini API reads it
+    # we don't save it in the same manner. Instead we save the json file with role and text
     for messages in chat.get_history():
         text_content = "".join([part.text for part in messages.parts if part.text])
         role = messages.role
@@ -45,6 +47,9 @@ def save_history(chat):
     with open(HISTORY_FILE, 'w') as hf:
         json.dump(chat_history, hf, indent=2)
 
+# here we are loading the pre-saved history (if it exists)
+# as the chat_history.json contains json schema, not the types.Content() objects,
+# we convert that json back into the form the Gemini API can read, the types.Content() objects
 def load_saved_history():
     if not HISTORY_FILE.exists():
         return []
@@ -67,20 +72,24 @@ def load_saved_history():
         print("Cannot load history!")
         return []
 
+# loading pre-saved history
 past_history = load_saved_history()
 
+# creating a chat and passing the pre-saved history
 chat = client.chats.create(
         model='gemini-3.1-flash-lite',
         history=past_history
     )
 
+# creating chatting function that sends message and prints response chunk by chunk
 def chatting(user_input):
-    
+
     response = chat.send_message_stream(
         message=user_input,
         config=config
     )
-
+    
+    print("=> AI: ")
     for chunk in response:
         print(chunk.text)
 
@@ -88,6 +97,8 @@ def chatting(user_input):
     #     print("ROLE: ", message.role)
     #     print("TEXT: ", message.parts[0].text)
 
+# here we use a while loop that runs until user says quit or bye
+# after they say quit or bye, the save_history() saves the conversation
 while True:
     user_input = input("=> YOU: ").strip()
 
