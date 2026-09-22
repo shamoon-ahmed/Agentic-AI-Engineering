00db44892362825e192f9dab50173212b53573b5
shamoon-ahmed <samsonrizzy6@gmail.com>
2026-09-22 23:58:52 +0500

    added comments

--- PHASE_03/fastapi-project/main.py
+++ PHASE_03/fastapi-project/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/") # @app = path operation decorator, .get() = path operation
 async def root(name: str): # path operation function
     return {"message": f"Hello {name}!"}
+
+# after deploying with: fastapi deploy
+# the app is available at: https://say-hello.fastapicloud.dev/
+# say-hello is the directory I named it when configuring the deployment in the terminal
