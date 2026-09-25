aed8fafb76515170ac55c2d8e8509c198cdfaa3a
shamoon-ahmed <samsonrizzy6@gmail.com>
2026-09-25 15:42:49 +0500

    updated with Body() parameters explanation

--- PHASE_03/fastapi-project/04_request_body.py
+++ PHASE_03/fastapi-project/04_request_body.py
@@ -22,7 +22,7 @@ class ItemUpdate(BaseModel):
     tax: float | None = None
 
 @app.post("/items/") # .post() used to create item
-def create_item(item: Item):
+def create_item(item: Item): # here item is a Body() parameter. Explanation at the end
     item_details = item.model_dump()
     
     if item.tax:
@@ -38,3 +38,18 @@ def update_item(item_id: str, item: Item):
 def update_item_details(item_id: str, item: ItemUpdate):
     updated_data = item.model_dump(exclude_unset=True) # excluding what's unset
     return {"item_id":item_id, "updated_item": updated_data}
+
+# so there are three types of parameter signatures essentially:
+# - Path() parameters
+# - Query() parameters
+# - Body() parameters
+
+# Let me explain Body() parameters
+# So for example when creating an account, uploading a blog post, etc. we don't send the entire payload in the URL
+# www.example.com/signup/?user=sam&password=1122&age=21&bio=live-in-pak
+# so this is a very bad approach, not suitable
+# instead these parameters are considered as Body() parameters which are not displayed in the URL and FastAPI knows that
+# FastAPI knows which parameters are Body(), Path() or Query() by looking at where it's coming from
+# if a parameter has a Pydantic model type like above "item: Item", FastAPI knows its a Body() parameter
+# but lets say we want just one parameter with a simple data type like "importance: int" to be a Body() parameter
+# we define it explicitly saying "importance: int = Body()"
